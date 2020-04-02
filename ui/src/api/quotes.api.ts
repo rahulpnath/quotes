c285cc27ab3fdbc893dab2513bb5bdf75ed0e3ac
Vincent Zhao <vincentzhao@readify.net>
2020-04-02 11:42:38 +1000

    Add new quote component

--- ui/src/api/quotes.api.ts
+++ ui/src/api/quotes.api.ts
@@ -1,7 +1,12 @@
-import { QuoteSummaryDto } from './api-models';
+import { QuoteDto, QuoteSummaryDto } from './api-models';
 import http from './http';
 
 export async function loadAllQuotes(): Promise<QuoteSummaryDto[]> {
   const response = await http.get<QuoteSummaryDto[]>('/api/quotes');
+  return response.data;
+}
+
+export async function loadQuote(id: string): Promise<QuoteDto> {
+  const response = await http.get<QuoteDto>(`/api/quote/${id}`);
   return response.data;
 }
---
+++ ui/src/views/routes/quotes/Quote.tsx
@@ -0,0 +1,43 @@
+import { QuoteDto, QuoteStatusCode } from 'api/api-models';
+import * as QuotesApi from 'api/quotes.api';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { LoadingPane } from 'views/components/application/LoadingPane';
+import { PageLayout } from 'views/components/application/PageLayout';
+import styles from './Quote.module.scss';
+import { QuoteStatus } from './QuoteStatus';
+
+export const Quote: React.FC = () => {
+  const { quoteId } = useParams();
+  const [quote, setQuote] = useState<QuoteDto | null>(null);
+  const loadQuote = async () => {
+    setQuote(await QuotesApi.loadQuote(quoteId || ''));
+  };
+
+  const title = quoteId
+    ? quote?.statusCode === QuoteStatusCode.Draft
+      ? 'Draft Quote'
+      : `Quote ${quote?.quoteNumber}`
+    : 'New Quote';
+
+  const quoteStatus = quoteId && quote && <QuoteStatus statusCode={quote.statusCode}></QuoteStatus>;
+
+  const audit = quote ? (
+    <span>
+      <strong>Last Updated: </strong>
+      <span>{quote.lastModifiedAt.toLocaleString()}</span>
+    </span>
+  ) : null;
+
+  return (
+    <PageLayout
+      title={title}
+      subtitle={quoteStatus}
+      parent={['All Quotes', '/quotes']}
+      audit={audit}>
+      <LoadingPane isLoading={false}>
+        <div className={styles.content}></div>
+      </LoadingPane>
+    </PageLayout>
+  );
+};
